8455a6e713a672a14aecaa49a447234cf358d3f2
Jostein Løvbråten <jostein.lovbraten@gmail.com>
2020-02-29 14:15:09 +0100

    prototype is finished

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, flash, redirect, url_for, send_file, request
+import json, random
+from flask import render_template, flash, redirect, url_for, send_file, request, make_response, Response
 from app import app
 from app.forms import MalwareForm, SerialForm, ACHForm
 
@@ -16,98 +17,10 @@ DOWNLOAD_FOLDER = os.path.dirname(os.path.realpath(__file__)) + "/files/"
 GIT_FOLDER = os.path.dirname(os.path.realpath(__file__)) + "/files/flashcards/"
 LOG_FOLDER = os.path.dirname(os.path.realpath(__file__)) + "/logs/"
 
-#File names
-SERIAL_NUMER_FILE_NAME = "serial_number.txt"
-
-#Git
-USER_NAME = "Jostein Løvbråten"
-USER_MAIL = "jostein.lovbraten@cert.no"
 
 
 
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
-def index():
-    form = MalwareForm()
-    serial_number = get_next_serial_number()
-    if form.validate_on_submit():
-        flash('Flashcard for {} with serial number {} has been submitted!'.format(form.serial_number.data, form.name.data.upper()))
-        
-        #Populate dictionary with data provided by the user
-        meta = {}
-        
-        meta["timestamp_created"] = int(time.time()) #epoch
-        meta["timestamp_modified"] = meta["timestamp_created"]
-        meta["serial_number"] = form.serial_number.data
-        meta["version"] = FORMAT_VERSION
-        meta["author"] = form.author.data
-        
-        
-        flashcard = {}
-        flashcard["meta"] = meta
-        flashcard["name"] = form.name.data.upper()
-        
-        flashcard["type"] = form.type.data
-        flashcard["communications"] = form.communication.data.splitlines()
-        flashcard["aliases"] = form.alias.data.splitlines()
-        flashcard["impacts"] = (form.impact.data).split(",")[:-1]
-        
-        flashcard["operating_systems"] = form.operative_system.data.splitlines()
-        flashcard["application"] = form.application.data
-        flashcard["vulnerabillity"] = form.vulnerabillity.data
-        
-        flashcard["capabillities"] = (form.key_capabillities.data).split(",")[:-1]
-        
-        flashcard["previous_incidents"] = form.previous_incidents.data.splitlines()
-        flashcard["require_user_interraction"] = form.require_user_interaction.data
-        flashcard["persistence"] = form.persistence.data
-        
-        flashcard["key_properties"] = form.key_properties.data.splitlines()
-        flashcard["key_indicators"] = form.key_indicators.data.splitlines()
-        
-        flashcard["threat_actor"] = form.threat_actor.data
-        flashcard["sources"] = form.sources.data.splitlines()
-        flashcard["git_link"] = form.git_link.data.splitlines()
-        
-        flashcard["description"] = form.description.data
-        
-        
-        #Create the file to be served to the user
-        filename = GIT_FOLDER + form.name.data + ".json"
-        file_to_serve = open(filename, "w")
-        
-        #Convert the dict to JSON and write the data to file
-        file_to_serve.write(dumps(flashcard))
-        
-        file_to_serve.close()
-        
-        pull_from_git()
-        commit_to_git("Added flashcard for " + flashcard["name"])
-        commit_serial_number(meta["serial_number"])
-
-        
-        
-
-    return render_template('index.html', title='flashGen', form=form, serial_number = serial_number)
-
-@app.route('/serial_number', methods=['GET','POST'])
-def serial_number():
-    form = SerialForm()
-    current_serial_number = get_serial_number()
-    next_serial_number = get_next_serial_number()
-    
-    if form.validate_on_submit():
-        flash('Serial number {}  claimed!'.format(next_serial_number))
-        commit_serial_number(next_serial_number)
-        return redirect('/serial_number')
-
-    
-    return render_template('serial_number.html', title='Serial Number', current_serial_number = current_serial_number, next_serial_number = next_serial_number, form=form)
-
-@app.route('/edit_json', methods=['GET','POST'])
-def edit_json():
-    pass
-    
 @app.route('/mhg', methods=['GET', 'POST'])
 def mhg():
     form = ACHForm()
@@ -123,17 +36,38 @@ def mhg():
         for r in product(who, what, why):
             generated_hypotheses.append(r)
 
-        messages = generated_hypotheses
-        session['messages'] = generated_hypotheses
-        return redirect(url_for("eliminate_hypothesis", messages=messages))
+        random.shuffle(generated_hypotheses)
+
+        resp = make_response(redirect(url_for('hypothesis_elimination')))
+        resp.set_cookie("hypos", json.dumps(generated_hypotheses))
+        return resp
         
         #Populate dictionary with data provided by the user
     return render_template('mhg.html', title='MHG', form=form)
 
 @app.route('/hypothesis_elimination', methods=['GET', 'POST'])
-def eliminate_hypothesis():
-    messages = session['messages']
-    return render_template("foo.html", messages=json.loads(messages))
+def hypothesis_elimination():
+    all_hypos = json.loads(request.cookies.get("hypos"))
+    print(all_hypos)
+
+    return render_template('hypothesis_elimination.html', title='elimination', all_hypos=str(all_hypos))
+
+@app.route('/scored', methods=['GET', 'POST'])
+def scored():
+    finished = []
+    all_scored = request.cookies.get("scoreboard")[:-1].split(',')
+    for entry in all_scored:
+        hypo = str(entry).split(":")
+        finished.append([hypo[0],hypo[1].strip()])
+    
+    finished.sort(reverse=True)
+
+    for h in finished:
+        print(h)
+    
+
+
+    return render_template('scored.html', title='finished!', finished=finished)
 
 def get_serial_number():
     current_year = datetime.datetime.now().year
@@ -150,56 +84,4 @@ def get_serial_number():
         serial_number = "REM#" + str(current_year) + "-000"
     
     return serial_number
-    
-def get_next_serial_number():
-    serial_number = get_serial_number()
-    serial_year = re.search('REM#(\d{4}).*', serial_number).group(1)
-    serial_delivery = int(re.search('REM#\d{4}-(\d*)', serial_number).group(1))
-    
-    serial_delivery += 1
-    
-    if serial_delivery < 10:
-        serial_delivery = '00' + str(serial_delivery)
-    elif serial_delivery >= 10 & serial_delivery < 100:
-        serial_delivery = '0' + str(serial_delivery)
-    else:
-        serial_delivery = str(serial_delivery)
-    
-    return "REM#" + serial_year + '-' + serial_delivery
-
-def commit_serial_number(serial_number):
-    path_to_serial_number = GIT_FOLDER + SERIAL_NUMER_FILE_NAME
-    file_handle = open(path_to_serial_number, "w")
-    
-    file_handle.write(serial_number)
-    file_handle.close()
-    
-    message = 'Incremented serial_number to ' + serial_number
-    commit_to_git(message)
-    push_to_git()
-
-def pull_from_git():
-    proc1 = subprocess.Popen(['git', '-C', GIT_FOLDER, 'pull'], stdout=subprocess.PIPE)
-    
-    result = proc1.communicate()
-    app.logger.info(result)
-    
-def commit_to_git(mess):
-    message = mess
-    proc1 = subprocess.Popen(['git', '-C', GIT_FOLDER, 'add', '.'], stdout=subprocess.PIPE)
-    proc2 = subprocess.Popen(['git', '-C', GIT_FOLDER, 'commit', '-m', message], stdout=subprocess.PIPE)
-    
-    result1 = proc1.communicate()
-    result2 = proc2.communicate()
-    
-    app.logger.info(result1)
-    app.logger.info(result2)
-    
-def push_to_git():
-    proc1 = subprocess.Popen(['git', '-C', GIT_FOLDER, 'push'], stdout=subprocess.PIPE)
-    
-    result = proc1.communicate()
-    app.logger.info(result)
-    
-    
     
